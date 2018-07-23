ffe11492ee8a5e7f6cba14d05083c8790f989b50
Ian Carpenter <ianhcarpenter@gmail.com>
2018-07-23 09:34:55 +0100

    Initial Commit

---
+++ IrisClustering/IrisData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IrisClustering
+{
+    class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        public float SepalWidth;
+
+        [Column("2")]
+        public float PetalLength;
+
+        [Column("3")]
+        public float PetalWidth;
+    }
+
+    class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
--- IrisClustering/Program.cs
+++ IrisClustering/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace IrisClustering
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PredictionModel<IrisData, ClusterPrediction> model = Train();
+
+            await model.WriteAsync(_modelPath);
+
+            var prediction = model.Predict(TestIrisData.Setosa);
+
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+        }
+
+        private static PredictionModel<IrisData, ClusterPrediction> Train()
+        {
+            var pipeline = new LearningPipeline
+            {
+                new TextLoader(_dataPath).CreateFrom<IrisData>(separator: ','),
+
+                new ColumnConcatenator(
+                "Features",
+                "SepalLength",
+                "SepalWidth",
+                "PetalLength",
+                "PetalWidth"),
+
+                new KMeansPlusPlusClusterer() { K = 3 }
+            };
+
+            var model = pipeline.Train<IrisData, ClusterPrediction>();
+
+            return model;
+
         }
     }
 }
